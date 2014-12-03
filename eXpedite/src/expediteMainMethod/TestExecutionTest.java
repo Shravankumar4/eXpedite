b0fbb7b773322e16181f421b0e28c12162eb43c6
shravankumar4 <shravan.kumar4@hp.com>
2014-12-03 14:11:34 +0530

    Added the syso4 files

--- eXpedite/src/expediteMainMethod/TestExecutionTest.java
+++ eXpedite/src/expediteMainMethod/TestExecutionTest.java
@@ -6,6 +6,12 @@ public class TestExecutionTest extends TestCase {
 
     public void testMain() throws Exception {
 
+
         System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+
+
     }
 }
